11cb2454a1154be08a6fb0a23e3e4a0d745792fe
Talha-Jubaer-Prantor <talhajubaer3020@gmail.com>
2022-12-14 12:39:09 +0600

    styling

--- src/components/Admin/AdminProduct/AdminProduct.js
+++ src/components/Admin/AdminProduct/AdminProduct.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import AdminNav from '../AdminNav/AdminNav';
 import "./AdminProduct.css"
 
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import "./Cart.css"
 import OtherNavbar from "../Navbar/OtherNavbar";
 
